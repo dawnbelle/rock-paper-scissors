02e082c83d2cad4d36129859a291f9f0ca716f50
Ray Belle <>
2022-07-13 17:54:46 -0700

    v1.0 of RPS

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ def main():
 
 def show_header():
     print("---------------------------")
-    print("   Rock Paper Scissors")
+    print("   Rock Paper Scissors - RPS")
     print("---------------------------")
 
 
